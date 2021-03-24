62aff8fab2f4937589306f48ab694481969e4b39
HolynekK <e238109@metu.edu.tr>
2021-03-25 02:26:59 +0300

    finilized

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,16 @@ var moment = require('moment');
 
 const messages = [
   {
-    title: "Hello Moruk",
-    user: "Mert Kaan",
-    text: "ahhaahahhaahahah",
+    title: "Hello Dude",
+    user: "HolyNekK",
+    text: "I love programming because of it's endless applications, you wish for something and programming can made it possible if you have enough resources and skill, second thing I love about programming is that, it build your problem solving skill which increases your creativity, and when you become more creative for a thing, you explore it, you love it, and you dream about it.",
     day: moment().format('LLL'),
   },
   {
-    title: "Yo I'm with ma boois",
-    user: "Ahmet Mehmet",
-    text: "heueheuhuehe",
-    date: moment().format('LLL'),
+    title: "Hello My Friend",
+    user: "HolyMoly",
+    text: "helLo DUDE. I hopE yoU ArE doiNg WeLL, bECAuSE i do. IF YOU dON't, just Leave ME A mESsaGE. Me ANd THE boIZ lovE YoU <3",
+    day: moment().format('LLL'),
   }
 ];
 
